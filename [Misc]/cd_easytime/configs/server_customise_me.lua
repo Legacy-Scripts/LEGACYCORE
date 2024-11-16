3e7ff8c6f6ba1068ea01213e739b16fc882d66d4
ENT510 <145626625+ENT510@users.noreply.github.com>
2024-11-16 14:00:52 +0100

    Add files via upload

---
+++ [Misc]/cd_easytime/configs/server_customise_me.lua
@@ -0,0 +1,58 @@
+function Notification(source, notif_type, message, title, icon, duration, tipo)
+    if source and notif_type and message then
+        if Config.NotificationType.client == 'esx' then
+            TriggerClientEvent('esx:showNotification', source, message)
+        
+        elseif Config.NotificationType.client == 'qbcore' then
+            if notif_type == 1 then
+                TriggerClientEvent('QBCore:Notify', source, message, 'success')
+            elseif notif_type == 2 then
+                TriggerClientEvent('QBCore:Notify', source, message, 'primary')
+            elseif notif_type == 3 then
+                TriggerClientEvent('QBCore:Notify', source, message, 'error')
+            end
+        
+        elseif Config.NotificationType.client == 'mythic_old' then
+            if notif_type == 1 then
+                TriggerClientEvent('mythic_notify:client:SendAlert:custom', source, { type = 'success', text = message, length = 10000})
+            elseif notif_type == 2 then
+                TriggerClientEvent('mythic_notify:client:SendAlert:custom', source, { type = 'inform', text = message, length = 10000})
+            elseif notif_type == 3 then
+                TriggerClientEvent('mythic_notify:client:SendAlert:custom', source, { type = 'error', text = message, length = 10000})
+            end
+
+        elseif Config.NotificationType.client == 'mythic_new' then
+            if notif_type == 1 then
+                TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'success', text = message, style = { ['background-color'] = '#ffffff', ['color'] = '#000000' } })
+            elseif notif_type == 2 then
+                TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = message, style = { ['background-color'] = '#ffffff', ['color'] = '#000000' } })
+            elseif notif_type == 3 then
+                TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = message, style = { ['background-color'] = '#ffffff', ['color'] = '#000000' } })
+            end
+
+        elseif Config.NotificationType.client == 'chat' then
+            TriggerClientEvent('chatMessage', source, message)
+
+        elseif Config.NotificationType.client == 'other' then
+            local oxType = 'inform'
+            
+            if notif_type == 1 then
+                oxType = 'success'
+            elseif notif_type == 2 then
+                oxType = 'inform'
+            elseif notif_type == 3 then
+                oxType = 'error'
+            end
+            
+          
+            TriggerClientEvent('ox_lib:notify', source, {
+                title = title or 'Legacy Core',
+                description = message,
+                icon = icon or 'inform',
+                duration = duration or 5000,
+                position = 'top',
+                type = oxType,
+            })
+        end
+    end
+end
